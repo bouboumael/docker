ab895bde8e248964f1e00b97029430ddc84d65f9
bouboumael <bouboumael@orange.fr>
2021-10-21 14:17:39 +0200

    gitignore

--- startup.sh
+++ startup.sh
@@ -85,7 +85,7 @@ sudo docker-compose exec -T php-service php bin/console d:d:c --no-interaction
 sudo docker-compose exec -T php-service php bin/console d:m:m --verbose --no-interaction --allow-no-migration
 
 if [ ${APP_ENV} != "prod" ]; then
-  sudo docker-compose exec -T php-service php bin/console doctrine:fixtures:load --quiet --no-interaction --no-debug
+    sudo docker-compose exec -T php-service php bin/console doctrine:fixtures:load --quiet --no-interaction --no-debug
 fi
 
 clear
