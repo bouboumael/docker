f08e75b3b1291d9c42545149f48f3fb9a68f9d9b
bouboumael <bouboumael@orange.fr>
2021-10-21 16:50:04 +0200

    debug

--- docker/php/docker-entry.sh
+++ docker/php/docker-entry.sh
@@ -3,6 +3,7 @@
 ## Symfony cconfiguration
 php bin/console doctrine:database:create --if-not-exists --quiet --no-interaction
 php bin/console doctrine:migrations:migrate --verbose --no-interaction --allow-no-migration
+
 if [ ${APP_ENV} != "prod" ]; then
   php bin/console doctrine:fixtures:load --quiet --no-interaction --no-debug
 fi
@@ -10,8 +11,8 @@ fi
 php bin/console cache:clear
 php bin/console cache:warmup
 
-chmod -R 777 /var/www/var
-chmod -R 777 /var/www/public
+chmod -R 777 /var
+chmod -R 777 /public
 
 ## server config
 php-fpm &
