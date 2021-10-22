6b7972d87f4aaa4ad938675e39ff3f3cb7e60e0d
bouboumael <bouboumael@orange.fr>
2021-10-22 09:07:24 +0200

    restore before composer

--- docker/php/docker-entry.sh
+++ docker/php/docker-entry.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 ## Symfony cconfiguration
-php bin/console doctrine:database:create --if-not-exists --quiet --no-interaction
-php bin/console doctrine:migrations:migrate --verbose --no-interaction --allow-no-migration
 
-php bin/console cache:clear
-php bin/console cache:warmup
+# php bin/console doctrine:database:create --if-not-exists --quiet --no-interaction
+# php bin/console doctrine:migrations:migrate --verbose --no-interaction --allow-no-migration
 
-chmod -R 777 /application
-chmod -R 777 /application/public
+# php bin/console cache:clear
+# php bin/console cache:warmup
+
+# chmod -R 777 /application
+# chmod -R 777 /application/public
