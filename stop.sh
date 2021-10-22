bdade42290a744f2baf43ffea226ee431f82a9f6
bouboumael <bouboumael@gmail.com>
2021-10-22 21:26:52 +0200

    entrypoint

--- stop.sh
+++ stop.sh
@@ -1,10 +1,12 @@
+#!/bin/sh
+
 sudo true
 
 read -p "Voulez-vous stopper et reset le server ? (N/o) : " stop
 
 sudo docker-compose down
 
-if [ $stop = 'o' ]
+if [ "$stop" = 'o' ]
 then
     sudo docker system prune -a
 fi
