5af4c6c01d75c82a44babb7f7528a16be19a487c
bouboumael <bouboumael@gmail.com>
2021-10-24 12:03:49 +0200

    grp

--- startup.sh
+++ startup.sh
@@ -36,15 +36,19 @@ then
     sudo curl -L "https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 
-    echo -e "\n"
-    echo "+-----------------------------------+"
-    echo "|  Configuration permission docker  |"
-    echo "+-----------------------------------+"
-    echo -e "\n"
+    egrep -i "^docker" /etc/group;
+    if [ $? -ne 0 ]
+    then
+        echo -e "\n"
+        echo "+-----------------------------------+"
+        echo "|  Configuration permission docker  |"
+        echo "+-----------------------------------+"
+        echo -e "\n"
 
-    sudo groupadd docker
-    sudo usermod -aG docker $USER
-    newgrp docker
+        sudo groupadd docker
+        sudo usermod -aG docker $USER
+        newgrp docker
+    fi
 fi
 
 echo -e "\n"
